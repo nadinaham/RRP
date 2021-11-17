4eb5c397d89b64e4315314f6244ce5c6dbb234d9
ZhuMacPro <zhumacpro@dhcp-10-250-49-14.harvard.edu>
2021-11-17 10:59:37 -0500

    background deletion

---
+++ background_deletion.py
@@ -0,0 +1,40 @@
+import numpy as np
+import glob
+import matplotlib.pyplot as plt
+import skimage.io
+import skimage.color
+import skimage.filters
+%matplotlib widget
+
+# load the image
+image = skimage.io.imread("../../fig/06-junk-before.png")
+
+fig, ax = plt.subplots()
+plt.imshow(image)
+plt.show()
+
+# convert the image to grayscale
+gray_image = skimage.color.rgb2gray(image)
+
+# blur the image to denoise
+blurred_image = skimage.filters.gaussian(gray_image, sigma=1.0)
+
+fig, ax = plt.subplots()
+plt.imshow(blurred_image, cmap='gray')
+plt.show()
+
+# create a mask based on the threshold
+t = 0.8
+binary_mask = blurred_image < t
+
+fig, ax = plt.subplots()
+plt.imshow(binary_mask, cmap='gray')
+plt.show()
+
+# use the binary_mask to select the "interesting" part of the image
+selection = np.zeros_like(image)
+selection[binary_mask] = image[binary_mask]
+
+fig, ax = plt.subplots()
+plt.imshow(selection)
+plt.show()
